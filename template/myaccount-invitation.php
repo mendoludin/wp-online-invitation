e36b5675082d39300a0f64ed0e0cfbba09a2d2fb
Muhamad Aminnudin <aminn1998@gmail.com>
2025-03-03 14:26:00 +0700

    Update myaccount-invitation.php

--- template/myaccount-invitation.php
+++ template/myaccount-invitation.php
@@ -1,1 +1,8 @@
-
+<?php
+if (is_user_logged_in()) {
+    $user_id = get_current_user_id();
+    echo '<h2>My Invitations</h2>';
+    // Tambahkan logik untuk menampilkan order dan field ACF di sini
+} else {
+    echo 'Please log in to view your invitations.';
+}
